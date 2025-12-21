050eac2f3e4308edac4837ace52dacfa72028273
Marco07811 <marcosalvatierra028@gmail.com>
2025-12-21 13:46:09 -0400

    Quinta carga

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://marck028.github.io/Pagina-Javi/',
+    site: 'https://marck028.github.io',
     base: '/Pagina-Javi/',
     trailingSlash: 'always', // <- importante para GitHub Pages
 });
